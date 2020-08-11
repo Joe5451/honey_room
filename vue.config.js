c82ce8cf4c82481e59cb81c25e95b08683fb2594
Joe <joeblack5451@gmail.com>
2020-08-11 23:25:25 +0800

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/honey_room/'
+//     : '/'
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/honey_room/'
-    : '/'
+  publicPath: './'
 }
